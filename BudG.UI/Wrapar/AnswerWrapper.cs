d2bf857ea48199d357c96e5334165244024b9df3
Mahmoud Samy <mahmodsamy102@gmail.com>
2024-06-23 19:29:57 +0300

    Create Answer Wrapper

---
+++ BudG.UI/Wrapar/AnswerWrapper.cs
@@ -0,0 +1,38 @@
+﻿using BudG.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudG.UI.Wrapar
+{
+  public class AnswerWrapper :ModelWrapper<Answer>
+    {
+        public AnswerWrapper(Answer model):base(model)
+        {
+            
+        }
+
+        public int Id { get { return Model.AnswerId; } }
+        public string AnswerQuestion
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+
+        public int QuesId
+        {
+            get { return GetValue<int>(); }
+            set { SetValue(value); }
+        }
+        public int UserId
+        {
+            get { return GetValue<int>(); }
+            set { SetValue(value); }
+        }
+
+        
+
+    }
+}
