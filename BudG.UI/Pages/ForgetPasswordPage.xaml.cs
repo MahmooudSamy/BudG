babaa807ef51f16de0b17f97f84490dbf9a3d59a
Mahmoud Samy <165038050+MahmooudSamy@users.noreply.github.com>
2025-11-13 16:00:33 +0200

    Add Pwassword and validation Email

---
+++ BudG.UI/Pages/ForgetPasswordPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BudG.UI.Pages
+{
+    /// <summary>
+    /// Interaction logic for ForgetPasswordPage.xaml
+    /// </summary>
+    public partial class ForgetPasswordPage : Page
+    {
+        private void storyboard()
+        {
+            Storyboard sb = Resources["SlideIn"] as Storyboard;
+            sb.Begin(ForgetPasswordGrid);
+        }
+        public ForgetPasswordPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
